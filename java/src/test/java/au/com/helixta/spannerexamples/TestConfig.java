49c0c2d3b7d6c00df8ca102d6acfa4d5adb5306e
Peter Runge <prunge@helixta.com.au>
2020-08-24 17:30:32 +1000

    Use Spanner emulator to run Java tests.

--- java/src/test/java/au/com/helixta/spannerexamples/TestConfig.java
+++ java/src/test/java/au/com/helixta/spannerexamples/TestConfig.java
@@ -1,23 +1,57 @@
 package au.com.helixta.spannerexamples;
 
 import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.DatabaseNotFoundException;
+import com.google.cloud.spanner.Instance;
+import com.google.cloud.spanner.InstanceAdminClient;
+import com.google.cloud.spanner.InstanceConfig;
+import com.google.cloud.spanner.InstanceId;
+import com.google.cloud.spanner.InstanceInfo;
+import com.google.cloud.spanner.InstanceNotFoundException;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Static configuration for tests.
  */
 public final class TestConfig
 {
+    public static final String EMULATOR_URI = System.getProperty("test.spanner.emulator");
+
     public static final DatabaseId DATABASE_ID;
 
     static
     {
-        String projectId = getProperty("test.projectId", "helix-sydney");
         String instanceId = getProperty("test.instanceId", "test");
         String databaseId = getProperty("test.databaseId", "test");
+        String projectId;
+        if (EMULATOR_URI != null)
+        {
+            try (Spanner spanner = SpannerOptions.newBuilder().setEmulatorHost(EMULATOR_URI).build().getService())
+            {
+                InstanceAdminClient instanceAdminClient = spanner.getInstanceAdminClient();
+                InstanceConfig instanceConfig = instanceAdminClient.listInstanceConfigs().iterateAll().iterator().next();
+                projectId = instanceConfig.getId().getProject();
+            }
+        }
+        else
+        {
+            projectId = getProperty("test.projectId", "helix-sydney");
+        }
         DATABASE_ID = DatabaseId.of(projectId, instanceId, databaseId);
     }
 
-    public static final String JDBC_URI = "jdbc:cloudspanner:/" + DATABASE_ID.getName();
+    public static final String JDBC_URI;
+    static
+    {
+        if (EMULATOR_URI == null)
+            JDBC_URI = "jdbc:cloudspanner:/" + DATABASE_ID.getName();
+        else
+            JDBC_URI = "jdbc:cloudspanner://" + EMULATOR_URI + "/" + DATABASE_ID.getName() + "?usePlainText=true";
+    }
 
     private TestConfig()
     {
@@ -32,5 +66,45 @@ public final class TestConfig
             value = defaultValue;
 
         return value;
+    }
+
+    public static void setUpEmulator()
+    throws InterruptedException, ExecutionException
+    {
+        if (EMULATOR_URI != null)
+        {
+            try (Spanner spanner = SpannerOptions.newBuilder().setEmulatorHost(EMULATOR_URI).build().getService())
+            {
+                InstanceAdminClient instanceAdminClient = spanner.getInstanceAdminClient();
+                InstanceConfig instanceConfig = instanceAdminClient.listInstanceConfigs().iterateAll().iterator().next();
+
+                //Create instance
+                Instance instance;
+                try
+                {
+                    instance = instanceAdminClient.getInstance(DATABASE_ID.getInstanceId().getInstance());
+                }
+                catch (InstanceNotFoundException e)
+                {
+                    instance = instanceAdminClient.createInstance(
+                            InstanceInfo.newBuilder(
+                                    InstanceId.of(instanceConfig.getId().getProject(), DATABASE_ID.getInstanceId().getInstance()))
+                                        .setNodeCount(1).setDisplayName("Test")
+                                        .setInstanceConfigId(instanceConfig.getId())
+                                        .build())
+                                                  .get();
+                }
+
+                //Create database if not already exists
+                try
+                {
+                    instance.getDatabase(DATABASE_ID.getDatabase());
+                }
+                catch (DatabaseNotFoundException e)
+                {
+                    instance.createDatabase(DATABASE_ID.getDatabase(), List.of()).get();
+                }
+            }
+        }
     }
 }
--- java/src/test/java/au/com/helixta/spannerexamples/music/SpannerIT.java
+++ java/src/test/java/au/com/helixta/spannerexamples/music/SpannerIT.java
@@ -1,6 +1,9 @@
-package au.com.helixta.spannerexamples.cats;
+package au.com.helixta.spannerexamples.music;
 
+import au.com.helixta.spannerexamples.TestConfig;
+import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,20 +18,28 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.concurrent.ExecutionException;
 
 import static au.com.helixta.spannerexamples.TestConfig.*;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = TestSpanner.AppConfiguration.class)
-class TestSpanner
+@ContextConfiguration(classes = SpannerIT.AppConfiguration.class)
+class SpannerIT
 {
-    private static final Logger log = LoggerFactory.getLogger(TestSpanner.class);
+    private static final Logger log = LoggerFactory.getLogger(SpannerIT.class);
 
     private static final SpannerSql spannerSql = new SpannerSql(JDBC_URI);
-    private static final SpannerMutations spannerMutations = new SpannerMutations(SpannerOptions.getDefaultInstance().getService(),
-                                                                                  DATABASE_ID);
+    private static final SpannerMutations spannerMutations =
+            new SpannerMutations(SpannerOptions.newBuilder().setEmulatorHost(EMULATOR_URI).build().getService(), DATABASE_ID);
     @Autowired
     private SpannerSpringData spannerSpringData;
+
+    @BeforeAll
+    static void setUpEmulator()
+    throws InterruptedException, ExecutionException
+    {
+        TestConfig.setUpEmulator();
+    }
 
     @BeforeEach
     void setUpDatabase()
@@ -67,6 +78,12 @@ class TestSpanner
         public DatabaseIdProvider databaseIdProvider()
         {
             return () -> DATABASE_ID;
+        }
+
+        @Bean
+        public Spanner spanner(SpannerOptions spannerOptions)
+        {
+            return spannerOptions.toBuilder().setEmulatorHost(EMULATOR_URI).build().getService();
         }
     }
 }
